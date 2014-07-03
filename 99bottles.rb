261510b916407f61aab61f5c3090a2d603c3919e
William Mahoney <wm.j.mahoney@gmail.com>
2014-07-03 06:17:59 -0400

    99 bottles of beer on the wall
    counts down and sings the song, all the way to 0.

---
+++ 99bottles.rb
@@ -0,0 +1,10 @@
+start = 99
+num = start
+
+while num >= 1
+	puts num.to_s + ' bottles of beer on the wall, ' +
+		num.to_s + ' bottles of beer'
+	num = num - 1
+	puts 'Take one down and pass it around, ' +
+		num.to_s + ' bottles of beer on the wall!'
+end
