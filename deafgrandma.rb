fcfb692ba652dfa3e74183b31ac0acbb52e2ac23
William Mahoney <wm.j.mahoney@gmail.com>
2014-07-03 06:18:39 -0400

    deaf grandma
    must yell to grandma, otherwise she asks you to speak up.  break with
    BYE

---
+++ deafgrandma.rb
@@ -0,0 +1,19 @@
+puts 'What would you like to say to your grandma?'
+
+
+while true
+	tograndma = gets.chomp
+	if tograndma == 'BYE'
+		puts 'GOOD BYE !!'
+		break
+	end
+	
+	
+	tograndma.downcase
+if tograndma == tograndma.upcase
+	random = 1930 + rand(21)
+	puts 'NO, NOT SINCE ' + random.to_s
+	else
+		puts 'HUH?! SPEAK UP, SONNY!'
+	end
+end
